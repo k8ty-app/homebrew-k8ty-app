cb5262e268032df96b7d06f91d405f449a8334b4
Mark Rudolph <mark@kangarootime.com>
2020-08-29 16:52:23 -0400

    Update k8ty-haiku to 0.0.2

--- Formula/k8ty-haiku.rb
+++ Formula/k8ty-haiku.rb
@@ -1,11 +1,14 @@
 class K8tyHaiku < Formula
   desc "Generate a haiku-like project name"
   homepage "https://github.com/k8ty-app/k8ty-haiku"
-  url "https://github.com/k8ty-app/k8ty-haiku/releases/download/v0.0.1/k8ty-haiku"
-  sha256 "291fe5e14a42722b360a843cd84acbf8afcfc4ede41646cb112d9577c1c3023c"
+  url "https://github.com/k8ty-app/k8ty-haiku/archive/v0.0.2.tar.gz"
+  sha256 "e252bf969f1dc784b40752c2196aa4ed9cb8556a6bde2dd01802edc394c86646"
+
+  depends_on "sbt"
 
   def install
-    bin.install "k8ty-haiku"
+    system "sbt" "nativeImage"
+    bin.install "./target/native-image/k8ty-haiku"
   end
 
   test do
